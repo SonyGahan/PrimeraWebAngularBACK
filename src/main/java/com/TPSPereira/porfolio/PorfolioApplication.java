03f694035f8e5698c395bb6d55bd08522bb19b3d
SonyGahan <94231488+SonyGahan@users.noreply.github.com>
2022-07-29 13:59:02 -0300

    modificacion error de cors

--- src/main/java/com/TPSPereira/porfolio/PorfolioApplication.java
+++ src/main/java/com/TPSPereira/porfolio/PorfolioApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.web.cors.CorsConfiguration;
 //import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-//import org.springframework.web.filter.CorsFilter;
+//git statusimport org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class PorfolioApplication {
@@ -22,7 +22,7 @@ public class PorfolioApplication {
 	//	CorsConfiguration corsConfiguration = new CorsConfiguration();
 	//	corsConfiguration.setAllowCredentials(true);
 	//	corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-	/	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+	//	corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 	//			"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
 	//			"Access-Control-Request-Method", "Access-Control-Request-Headers"));
 	//	corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Authorization",
